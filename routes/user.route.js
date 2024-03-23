47e26c391a68ce0bed70f6de6c8ef55a5043ba9f
naolleg <naollegesse51@gmail.com>
2024-03-23 23:49:42 +0300

    adding muter ln the request

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ const UserController = require('../controllers/user.controller')
 //request 
 userRoute.get('/admin/users',UserController.getUsers )
 // userRoute.get('/admin/users/:id',UserController.getsingleUsers)ssss
-userRoute.post('/signup',UserController.createUser)
+userRoute.post('/signup',upload.single("image"),UserController.createUser)
 // userRoute.post('/admin/addprovider',UserController.createprovider)
 // userRoute.delete('/admin/users/:id',UserController.deleteUser)
 // userRoute.put('/api/user/:id',UserController.updateUser)
