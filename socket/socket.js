6240b6e52318532727101f6c811107e7ee567112
naolleg <naollegesse51@gmail.com>
2024-03-27 22:08:38 +0300

    creating socket message chatting

--- socket/socket.js
+++ socket/socket.js
@@ -1,3 +1,16 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+  
+    socket.on('chat message', (message) => {
+    console.log('Received message:', message);
+    // Emit the message to all connected clients
+    io.emit('chat message', message);
+    });socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        });
+        });
