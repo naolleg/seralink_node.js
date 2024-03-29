54fe05a9eb1349873dd1d399561e0a568c7637aa
naolleg <naollegesse51@gmail.com>
2024-03-29 23:04:27 +0300

    query  o

--- querys/profile.query.js
+++ querys/profile.query.js
@@ -1,5 +1,5 @@
 const profilequery={
-    
+    updateProfile: `INSERT INTO users (userId, firstName, middleName, lastName, createdDate) VALUES (?,?,?,?,NOW());`, 
     getUserProfile:"SELECT JSON_OBJECT('email', email, 'firstname', firstname, 'lastname', lastname, 'role', role) AS json_data FROM users WHERE userId = ?",
 }
 module.exports=profilequery
